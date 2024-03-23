514c6da0ba70193fd68ef662c1bdc6a0b89285fc
Laura Lyons <maxwell6826@yahoo.co.uk>
2024-03-23 17:42:21 +0000

    more analysis

--- analysis.py
+++ analysis.py
@@ -11,12 +11,17 @@ import seaborn as sns
 df=pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv')
 
 # It would be helpful to know what type fo data each of the variables is?
-df.dtypes
+types= df.dtypes
+print(types)
 
 # Get a summary of each variable
 summary = df.describe()
 
 # Save the summary to a text file
-summary.to_csv("iris_summary.txt", sep="\t", index=False)
+#summary.to_csv("iris_summary.txt", sep="\t", index=False)
+#print(" A summary of each variable saved to iris_summary.txt")
 
-print(" A summary of each variable saved to iris_summary.txt")
+# It would be useful if we could get a summary of the variables, subdivided by the iris species.
+summary_by_species = df.groupby('species').describe()
+summary_by_species.to_csv("iris_summary_by_species.txt", sep="\t")
+print("A summary of each variable, subdivided by iris type, has been saved to iris_summary_by_species.txt")
