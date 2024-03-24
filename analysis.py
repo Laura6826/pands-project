5a52ef8e20d093c2ecd3a20d603a1cfd7567adc2
Laura Lyons <maxwell6826@yahoo.co.uk>
2024-03-24 18:55:44 +0000

    tweeking graphs

--- analysis.py
+++ analysis.py
@@ -18,14 +18,14 @@ print(types)
 summary = df.describe()
 
 # Save the summary to a text file
-#summary.to_csv("iris_summary.txt", sep="\t", index=False)
-#print(" A summary of each variable saved to iris_summary.txt")
+summary.to_csv("iris_summary.txt", sep="\t", index=False)
+print(" A summary of each variable saved to iris_summary.txt")
 
 # It would be useful if we could get a summary of the variables, subdivided by the iris species.
-summary_by_species = df.groupby('species').describe()
+'''summary_by_species = df.groupby('species').describe()
 summary_by_species.to_csv("iris_summary_by_species.txt", sep="\t")
-print("A summary of each variable, subdivided by iris type, has been saved to iris_summary_by_species.txt")plt.figure(figsize=(10, 6))
-
+print("A summary of each variable, subdivided by iris type, has been saved to iris_summary_by_species.txt")
+'''
 
 
 
