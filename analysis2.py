2c89a0d7dcf5e6de245e03460443a70bcd3e2d02
Laura Lyons <maxwell6826@yahoo.co.uk>
2024-03-24 19:06:57 +0000

    Histograms good

--- analysis2.py
+++ analysis2.py
@@ -1,4 +1,5 @@
-# This is an analysis of the Fisher Iris Data set.
+# This is an code analysis of the Fisher Iris Data set. 
+# More detail is available in the README and accompanying Jupyternotebook.
 # Author: Laura Lyons
 
 import seaborn as sns
@@ -8,22 +9,10 @@ import pandas as pd
 # Load the Iris dataset
 df=pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv')
 
-# Filter the data for the "versicolor" species
+# Filter the data by the species.
 versicolor_df = df[df["species"] == "versicolor"]
 setosa_df=df[df["species"] == "setosa"]
 virginica_df=df[df["species"] == "virginica"]
-
-# Extract data
-plen = df['petal_length']
-pwidth = df['petal_width']
-slen = df['sepal_length']
-swidth = df['sepal_width']
-
-# Fit a straight line between petal length and petal width
-# m1, c1 = np.polyfit(plen, pwidth, 1)
-
-# Fit a straight line between sepal length and sepal width
-# m2, c2 = np.polyfit(slen, swidth, 1)
 
 # Create a figure containing a subplot for all the variables, subdivided by iris species using subplots.
 fig, ax = plt.subplots(4,3, figsize=(15, 15))
@@ -96,6 +85,18 @@ ax[3,2].set_xlabel('Sepal Width (cm)')
 # Adjust spacing between subplots
 # Reference: https://www.geeksforgeeks.org/how-to-set-the-spacing-between-subplots-in-matplotlib-in-python/
 # Reference: https://stackoverflow.com/questions/6541123/improve-subplot-size-spacing-with-many-subplots
-plt.subplots_adjust(top=0.8, wspace=0.3, hspace=0.5)
+plt.subplots_adjust(top=0.905,bottom=0.136, left=0.097, right=0.970, wspace=0.239, hspace=0.910)
 plt.show()
 plt.savefig('Histogram of Petal Length (cm),subdivided by iris species.png')
+
+# Extract data
+plen = df['petal_length']
+pwidth = df['petal_width']
+slen = df['sepal_length']
+swidth = df['sepal_width']
+
+# Fit a straight line between petal length and petal width
+# m1, c1 = np.polyfit(plen, pwidth, 1)
+
+# Fit a straight line between sepal length and sepal width
+# m2, c2 = np.polyfit(slen, swidth, 1)
